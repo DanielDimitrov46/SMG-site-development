cd2def02462173f3aa7c49f9619faa59200da847
Daniel Dimitrov <dani.dimitrov.snack9@gmail.com>
2022-10-05 16:03:44 +0300

    fixing problems

--- SMG-Studios/header.php
+++ SMG-Studios/header.php
@@ -7,38 +7,54 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SMG</title>
-    <link rel="icon" type="image/x-icon" href="images/SMHjpg.jpg">
+    <link rel="icon" type="image/x-icon" href="images/da%20te%20eba.png">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 </head>
 <body>
 
 <!--- NAVBAR -->
-<div class = "banner" style="position: fixed; width: 100%; z-index: 500; ">
-    <div class = "navbar" style="box-shadow: 2px 1px 10px gray; ">
+<!-- Load an icon library to show a hamburger menu (bars) on small screens -->
+<div class="topnav" id="myTopnav">
+    <a href="#home" class="active">Home</a>
+    <a href="#news">News</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">About</a>
+    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+        <i class="fa fa-bars"></i>
+    </a>
+</div>
+<div class="col"
+        <a href = "index.php" class = "image-logo"><img src = "images/da%20te%20eba.png" class = "logo" style="width: 15%"></a>
+</div>
 
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#">Disabled</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+
+<!--<div class = "banner" style="position: fixed; width: 100%; z-index: 500; ">-->
+<!--    <div class = "navbar" style="box-shadow: 2px 1px 10px gray; ">-->
+<!---->
+<!--        <nav class="navbar navbar-expand-lg navbar-light bg-light">-->
+<!--            <a class="navbar-brand" href="#">Navbar</a>-->
+<!--            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">-->
+<!--                <span class="navbar-toggler-icon"></span>-->
+<!--            </button>-->
+<!--            <div class="collapse navbar-collapse" id="navbarNav">-->
+<!--                <ul class="navbar-nav">-->
+<!--                    <li class="nav-item active">-->
+<!--                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>-->
+<!--                    </li>-->
+<!--                    <li class="nav-item">-->
+<!--                        <a class="nav-link" href="#">Features</a>-->
+<!--                    </li>-->
+<!--                    <li class="nav-item">-->
+<!--                        <a class="nav-link" href="#">Pricing</a>-->
+<!--                    </li>-->
+<!--                    <li class="nav-item">-->
+<!--                        <a class="nav-link disabled" href="#">Disabled</a>-->
+<!--                    </li>-->
+<!--                </ul>-->
+<!--            </div>-->
+<!--        </nav>-->
 <!---->
 <!--        <div class="col">-->
 <!--            <a href = "index.php" class = "image-logo"><img src = "images/da%20te%20eba.png" class = "logo" style="width: 15%"></a>-->
@@ -61,3 +77,13 @@
 
 </body>
 </html>
+
+<script>/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+        }
+    }</script>
--- SMG-Studios/index.php
+++ SMG-Studios/index.php
@@ -1,11 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-<style>
-    .left{float:left;}
-    .right{float:right;}
-</style>
-
-
 <head>
     <link rel="stylesheet" href="style.css?<?php echo time(); ?>">
 
@@ -43,13 +37,13 @@
 <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel" >
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <img src="images/carousel/studio1.jpg" style="height: 1000px; width: 1000px; float: left">
+            <img src="images/carousel/studio1.jpg" style="height: 1000px; width: 100%;">
         </div>
         <div class="carousel-item">
-            <img src="images/carousel/studio2.jpg" style="height: 1000px; width: 1000px; float: left">
+            <img src="images/carousel/studio2.jpg" style="height: 1000px; width: 100%;">
         </div>
         <div class="carousel-item">
-            <img src="images/carousel/studio3.png" style="height: 600px; width: 1000px; float: left">
+            <img src="images/carousel/studio3.png" style="height: 600px; width: 100%;">
         </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
@@ -68,8 +62,8 @@
 <br><br>
 <div class="texts">
     <div class="first-quarter">
-        <p>jksdafnfjisndfnsdfbnw
-            lifnafbnbuifnasdnfjsnafnafn
+        <p>What is Lorem Ipsum?
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
         </p>
     </div>
     <div class="second-quarter">
@@ -79,7 +73,8 @@
         <img class = "neshto2" src="images/quarters/neshto2.jpg" alt="...">
     </div>
     <div class="fourth-quarter">
-        <p>sdffhjbbbbbbbhjsdfbhkjbsdfhjbsdhjfbsdjh</p>
+        <p>Why do we use it?
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).   </p>
     </div>
 </div>
 <br><br><br><br>
